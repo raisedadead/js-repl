a25739e28d8326b494b27ce4fd25bed57fb12c57
Mrugesh Mohapatra <me@raisedadead.com>
2017-11-22 14:55:13 +0530

    feat: inital commit

---
+++ index.js
@@ -0,0 +1,3 @@
+import main from "./main";
+console.log("\x1Bc");
+main();
---
+++ main.js
@@ -0,0 +1,8 @@
+const main = () => {
+  /**
+   * insert your code here.
+   */
+  console.log("hello simple repl");
+};
+
+export default main;
